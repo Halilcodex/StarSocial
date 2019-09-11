fcd0023a20c2c2e952cdeb2333d464d57d6221d5
halil <halil4deen@gmail.com>
2019-09-11 15:32:44 +0100

    added the STATIC_ROOT variable needed for heroku deployment

--- socialmedia/settings.py
+++ socialmedia/settings.py
@@ -133,6 +133,9 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR,'static')]
 
+# Needed for deployment in heroku
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 #MEDIA
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR,'media')
