df043250a3338e35665b032ba58974b1263ef9ac
halil <halil4deen@gmail.com>
2019-09-12 14:20:23 +0100

    setup my postgresql db and removed sqlite3 in settings.py

--- socialmedia/settings.py
+++ socialmedia/settings.py
@@ -86,8 +86,12 @@ WSGI_APPLICATION = 'socialmedia.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql-psycopg2',
+        'NAME': os.environ.get('DB_NAME', ''),
+        'USER': os.environ.get('DB_USER', ''),
+        'PASSWORD': os.environ.get('DB_PASS', ''),
+        'HOST': os.environ.get('DB_HOST', ''),
+        'PORT': os.environ.get('DB_PORT', ''),
     }
 }
 
