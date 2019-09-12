be7322df1a8547934b73d62509009e5e9eaf8f06
halil <halil4deen@gmail.com>
2019-09-12 10:00:33 +0100

    pip installed whitenoise, applied to settings.py and requirements.txt

--- socialmedia/settings.py
+++ socialmedia/settings.py
@@ -47,6 +47,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -133,6 +134,9 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR,'static')]
+
+# To use with whitenoise
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Needed for deployment in heroku
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
