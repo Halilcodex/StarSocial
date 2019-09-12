615b77961f174236a7bd4d86281c15c12515fba7
halil <halil4deen@gmail.com>
2019-09-12 11:42:26 +0100

    made some changes to the accounts app so as to get a success message after signing up

--- accounts/views.py
+++ accounts/views.py
@@ -1,12 +1,12 @@
-from django.shortcuts import render
+from django.contrib.messages.views import SuccessMessageMixin
 from django.views.generic import CreateView
 from . import forms
-from django.urls import reverse_lazy
-from django.contrib.messages.views import SuccessMessageMixin
+from django.urls import reverse_lazy, reverse
+
 
 # Create your views here.
 class SignUp(SuccessMessageMixin, CreateView):
     template_name = 'registration/signup.html'
     form_class = forms.UserSignupForm
-    success_url = reverse_lazy('accounts:login')
+    success_url = reverse_lazy('accounts:signup')
     success_message = "Sign up was successful"
