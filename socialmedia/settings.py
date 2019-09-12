d7559202666dfd2c7b55203d43b24cb2f1dc7d5f
halil <halil4deen@gmail.com>
2019-09-12 12:42:19 +0100

    pip installed django-heroku and applied in settings file

--- socialmedia/settings.py
+++ socialmedia/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -150,3 +151,6 @@ MEDIA_ROOT = os.path.join(BASE_DIR,'media')
 
 #LOGIN
 LOGIN_REDIRECT_URL = '/'
+
+
+django_heroku.settings(locals())
